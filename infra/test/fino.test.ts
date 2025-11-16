3ab0a252830ce960a89d97c5b1a7d9eea938f633
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-17 02:28:22 +0900

    cdk: init

---
+++ infra/test/fino.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Fino from '../lib/fino-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fino-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Fino.FinoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
