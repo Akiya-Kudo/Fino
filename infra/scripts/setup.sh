6201538fe381894384cab9b51a7ce1e340388a7a
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-17 00:38:36 +0900

    localstack: setting

---
+++ infra/scripts/setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Create AWS Account
+
+# Create AWS API Key
+# @see https://docs.localstack.cloud/aws/integrations/aws-native-tools/aws-cli/
+
+# Setup awscli (with the API Key)
+# @see https://docs.localstack.cloud/aws/integrations/aws-native-tools/aws-cdk/
+
+# Setup aws-cdk
+
+# Setup awscli-local
+
+# Setup aws-cdk-local
+
+# Setup Locakstack
+export AWS_PROFILE=localstack
