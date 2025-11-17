cdfcf67909fd7d35ed7c416785634f12555463a5
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-18 00:00:25 +0900

    biome:vscode-workspace: 設定

--- infra/bin/cdk.ts
+++ infra/bin/cdk.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { FinoStack } from '../lib/fino-stack';
+import * as cdk from "aws-cdk-lib/core";
+import { FinoStack } from "../lib/fino-stack";
 
 const app = new cdk.App();
-new FinoStack(app, 'FinoStack', {
-  env: {},
+new FinoStack(app, "FinoStack", {
+	env: {},
 });
--- infra/lib/fino-stack.ts
+++ infra/lib/fino-stack.ts
@@ -1,16 +1,9 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib/core";
+import type { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FinoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FinoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+	}
 }
