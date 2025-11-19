17af06bdeeacb693fc5e9b7ba8a3ff1000e98418
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-20 01:16:20 +0900

    fix: 不要なerror発火を削除

--- infra/bin/cdk.ts
+++ infra/bin/cdk.ts
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib/core";
 import { HothRawStorageStack } from "../lib/hoth/raw-storage";
-import { getTargetEnv } from "../lib/util/cdk/context";
 
 const app = new cdk.App();
-
-const targetEnv = getTargetEnv();
-console.log(targetEnv);
-throw new Error(targetEnv.toString());
 
 new HothRawStorageStack(app, {
 	env: {},
