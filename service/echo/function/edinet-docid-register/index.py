34cc7b6aab45e88589574271ed39254a07966e43
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-24 23:46:54 +0900

    dep: aws lambda powertoolsをinstall

--- service/echo/function/edinet-docid-register/index.py
+++ service/echo/function/edinet-docid-register/index.py
@@ -1,15 +1,22 @@
+from typing import Any, Dict
+
 import boto3
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+logger = Logger()
 
 dynamodb = boto3.resource("dynamodb")
 
 
-def handler(event, context):
+@logger.inject_lambda_context(log_event=True)
+def handler(event: EventBridgeEvent, context: LambdaContext) -> Dict[str, Any]:
     """
     # Edinet Document ID Register Lambda Function
 
     ## Description
     - eventから書類IDを取得し、DynamoDBのIngestion Stateに登録する
     """
-    print(event)
-    print(context)
+    logger.info(f"Event: {event}")
     return {"statusCode": 200, "body": "Hello, World!"}
