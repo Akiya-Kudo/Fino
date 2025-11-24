31b0a84f42a39a5342f67d43883a3393624dd649
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-24 23:01:04 +0900

    dep:lambda: uvで依存の追加

---
+++ service/echo/function/edinet-doc-ingestion/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from edinet-doc-ingestion!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ service/echo/function/edinet-docid-register/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from edinet-docid-register!")
+
+
+if __name__ == "__main__":
+    main()
