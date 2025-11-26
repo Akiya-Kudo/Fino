edc0e085478c44c2dccf06bc2d3159875e09ccb7
Akiya-Kudo <k.akiya07@gmail.com>
2025-11-26 20:37:45 +0900

    fix: python version

--- service/echo/function/edinet-docid-register/index.py
+++ service/echo/function/edinet-docid-register/index.py
@@ -5,6 +5,10 @@ from typing import Any, Dict
 import boto3
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.validation import validate
+from aws_lambda_powertools.utilities.validation.exceptions import SchemaValidationError
+
+
 
 logger = Logger()
 
@@ -14,6 +18,7 @@ LambdaEvent = {"detail": { "document_ids": list[str], "sec_code": str}, "detailT
 
 
 @logger.inject_lambda_context(log_event=True)
+@validate(event_schema=LambdaEvent)
 def handler(event: LambdaEvent, context: LambdaContext) -> Dict[str, Any]:
     """
     # Edinet Document ID Register Lambda Function
