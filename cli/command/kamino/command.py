cdb0f30599de8b0dd9fb923307aa27de7bbb37ab
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-10 02:35:45 +0900

    add: target option

--- cli/command/kamino/command.py
+++ cli/command/kamino/command.py
@@ -1,14 +1,32 @@
+from enum import Enum
+
 import typer
+from typing_extensions import Annotated
 
 app = typer.Typer(no_args_is_help=True)
 
 
+class Target(str, Enum):
+    EDINET = "edinet"
+    TDNET = "tdnet"
+
+
 @app.command()
-def collect():
+def collect(
+    target: Annotated[
+        Target,
+        typer.Option(
+            "--target",
+            "-t",
+            case_sensitive=False,
+            help="Target system name to collect data",
+        ),
+    ] = "edinet",
+):
     """
-    Collect data from the source.
+    Collect data from the target system.
     """
-    print("Collecting data from the source...")
+    print(target.value)
 
 
 if __name__ == "__main__":
