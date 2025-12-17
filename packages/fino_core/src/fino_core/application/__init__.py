ada676c0afa5d1fc6d813365d8c8744eadbd2ab1
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-17 23:49:52 +0900

    boom!!!!!!!!!!!!!!

--- packages/fino_core/src/fino_core/application/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Application layer for Fino Core."""
-
-from .usecases.collect_edinet import collect_edinet
-from .usecases.collect_tdnet import collect_tdnet
-
-__all__ = ["collect_edinet", "collect_tdnet"]
