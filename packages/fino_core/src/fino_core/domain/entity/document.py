9f21a6b2af9c9a59c0a92fdc760b033517150220
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-30 13:05:00 +0900

    delete: collector prefix dir

--- packages/fino_core/src/fino_core/domain/entity/document.py
+++ packages/fino_core/src/fino_core/domain/entity/document.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
-from fino_core.domain.value.collector import DocumentId, FilingFormat
+from fino_core.domain.entity.entity import AggregateRoot
+from fino_core.domain.value import DocumentId, FilingFormat
 
 from .document_metadata import DocumentMetadata
 
 
-@dataclass
-class Document:
+# @see: about dataclass annotation:  https://qiita.com/fumiya0238/items/46115b399c4ea1322ee8
+@dataclass(kw_only=True, slots=True, eq=False)
+class Document(AggregateRoot):
     """文書本体を表現するドメインエンティティ
 
     DocumentはDocumentMetadataを含み、実際の文書データ（resource）を保持します。
