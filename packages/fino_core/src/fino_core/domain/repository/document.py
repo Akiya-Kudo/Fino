06021764e30fe6e635d98fcc06318a16cbcdaeda
Akiya-Kudo <k.akiya07@gmail.com>
2025-12-31 23:13:45 +0900

    domain: criteriaを定義してみる

--- packages/fino_core/src/fino_core/domain/repository/document.py
+++ packages/fino_core/src/fino_core/domain/repository/document.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from fino_core.domain.entity.document import Document
 from fino_core.domain.value.document_id import DocumentId
+from fino_core.domain.value.document_search_criteria import DocumentSearchCriteria
 
 
 class DocumentRepository(ABC):
@@ -11,3 +12,6 @@ class DocumentRepository(ABC):
 
     @abstractmethod
     def save(self, document: Document, file: bytes) -> None: ...
+
+    @abstractmethod
+    def list(self, criteria: DocumentSearchCriteria) -> bool: ...
